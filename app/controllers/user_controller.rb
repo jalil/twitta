26156efa68108cd5bdc1148485feb6a047948eea
Jalil <jalilsan@gmail.com>
2010-12-12 17:45:33 -0800

    Initial commit

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,12 @@
+class UserController < ApplicationController
+
+  def show 
+	@user = User.find(params[:id])
+  end
+	
+
+  def new
+	@title = "Sign up"
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twitta::Application.config.secret_token = '26162b44f56f9a2328d8dc96042c47ef77d5a1c760cf72fc2e2122340a20f304cf201f82730d30e631f76740db83debad84353b0396eb892a0ceae9f10539d65'
