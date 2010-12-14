32df8af4247fa3ef2c41485a6f9efe1939dfd66e
Jalil <jalilsan@gmail.com>
2010-12-13 21:28:13 -0800

    Done with static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Twitta::Application.routes.draw do
    resources :users
-   match '/signup',  :to => 'users#new'
-
+   match '/signup',  :to => 'users#new' 
 
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
